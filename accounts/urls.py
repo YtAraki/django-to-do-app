2785aaba2de415c4df9b4646d9384ea32ecd9d44
“YtAraki” <yuto.araki@lt-s.jp>
2025-05-31 13:41:45 +0900

    initial commit

---
+++ accounts/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from django.contrib.auth.views import LogoutView
+
+from accounts.views import MyLoginView, SignUpView
+
+
+
+urlpatterns = [
+    path("signup/", SignUpView.as_view(), name="signup"),
+    path("Login/", MyLoginView.as_view(), name='login'),
+    path("logout/", LogoutView.as_view(), name='logout'),
+]
